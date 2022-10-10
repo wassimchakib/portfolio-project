3b8e39b4ad9d3709a15f44543464fb670b2350b7
Wassim <wassimchakib97@gmail.com>
2022-10-10 14:26:31 +0100

    Fix eslint and stylelint errors

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const navbarHamburger = document.querySelector('#navbar-hamburger');
-const mobileMenu = document.querySelector("#mobile-menu");
+const mobileMenu = document.querySelector('#mobile-menu');
 const cross = document.querySelector('#cross');
 navbarHamburger.addEventListener('click', () => {
-    mobileMenu.style.display = "block";
+  mobileMenu.style.display = 'block';
 });
 
 cross.addEventListener('click', () => {
-    mobileMenu.style.display = "none";
+  mobileMenu.style.display = 'none';
 });
 
 document.querySelectorAll('.nav-link').forEach(
-    (link) => {
-        link.addEventListener('click',() => {
-            mobileMenu.style.display = "none";
-        })
-    }
-)
+  (link) => {
+    link.addEventListener('click', () => {
+      mobileMenu.style.display = 'none';
+    });
+  },
+);
